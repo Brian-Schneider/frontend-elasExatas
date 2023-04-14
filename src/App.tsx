3621daadb7bbf8de1b342e48d4909d229c9af8fd
viniciusaprazeres <viniciusaprazeres@gmail.com>
2023-04-14 14:09:17 -0300

    Criação da página login

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+import Login from './paginas/login/Login'
+
 import './App.css'
 
 function App() {
 
   return (
-    <></>
+    <BrowserRouter>
+    <div style={{ minHeight: '100vh' }}>
+      <Routes>
+          <Route path='/' element={<Login />} />
+          <Route path='/login' element={<Login />} />
+      </Routes>
+    </div>
+  </BrowserRouter>
   )
 }
 
