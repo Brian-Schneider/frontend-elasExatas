a66e6b32aa85e597c44a2a83ff35575a13482587
Vinicius Prazeres <viniciusaprazeres@gmail.com>
2023-04-26 12:06:37 -0300

    Implementação de Cadastro e Listagem de Temas

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import Navbar from './components/estaticos/navbar/Navbar'
 import Footer from './components/estaticos/footer/Footer';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './paginas/login/Login'
+import ListaTemas from './components/temas/listaTemas/ListaTemas'
+import CadastroTema from './components/temas/cadastroTema/CadastroTema'
 
 function App() {
 
@@ -19,6 +21,9 @@ function App() {
           <Route path='/login' element={<Login />} />
           <Route path='/cadastrousuario' element={<CadastroUsuario />} />
           <Route path='/home' element={<Home />} />
+          <Route path='/temas' element={<ListaTemas />} />
+          <Route path='/cadastrartema' element={<CadastroTema />} />
+          <Route path='/editartema/:id' element={<CadastroTema />} />
       </Routes>
     </div>
     <Footer />
