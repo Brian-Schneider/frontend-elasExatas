b3c7bb93bcc2a524b98315d40829c3e400bdc130
Vinicius Prazeres <viniciusaprazeres@gmail.com>
2023-05-02 12:56:38 -0300

    Refatoração
    Retirada de importações não utilizadas e ajuste de margem na Box dos Cards

--- src/components/mulheres/Mulheres.tsx
+++ src/components/mulheres/Mulheres.tsx
@@ -6,14 +6,9 @@ import CardMedia from "@mui/material/CardMedia";
 import CardContent from "@mui/material/CardContent";
 import CardActions from "@mui/material/CardActions";
 import Collapse from "@mui/material/Collapse";
-import Avatar from "@mui/material/Avatar";
 import IconButton, { IconButtonProps } from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
-import { red } from "@mui/material/colors";
-import FavoriteIcon from "@mui/icons-material/Favorite";
-import ShareIcon from "@mui/icons-material/Share";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import MoreVertIcon from "@mui/icons-material/MoreVert";
 import { Box } from "@mui/material";
 
 interface ExpandMoreProps extends IconButtonProps {
@@ -76,6 +71,7 @@ export default function Mulheres() {
         justifyContent={"space-around"}
         flexWrap={"wrap"}
         gap={"40px"}
+        margin={'40px'}
       >
         <Box>
           <Card sx={{ maxWidth: 345 }}>
