a45883c432962eb0a1389cfc49bd821efc155e14
Vinicius Prazeres <viniciusaprazeres@gmail.com>
2023-05-01 21:06:48 -0300

    Atualização do Componente Mulheres
    Andrea Ghez

--- src/components/mulheres/Mulheres.tsx
+++ src/components/mulheres/Mulheres.tsx
@@ -38,77 +38,146 @@ export default function Mulheres() {
   };
 
   return (
-    <Card sx={{ maxWidth: 345 }}>
-      <CardHeader title="Maryam Mirzakhani" subheader="Matemática" />
-      <CardMedia
-        component="img"
-        height="194"
-        image="https://news.stanford.edu/wp-content/uploads/2017/07/Fields_Maryam_Mirzakhani.jpg"
-        alt="Paella dish"
-      />
-      <CardContent>
-        <Typography variant="body2" color="text.secondary">
-          “Você tem que gastar um pouco de energia e esforço para ver a beleza
-          da matemática.”
-        </Typography>
-      </CardContent>
-      <CardActions disableSpacing>
-        <ExpandMore
-          expand={expanded}
-          onClick={handleExpandClick}
-          aria-expanded={expanded}
-          aria-label="show more"
-        >
-          <ExpandMoreIcon />
-        </ExpandMore>
-      </CardActions>
-      <Collapse in={expanded} timeout="auto" unmountOnExit>
+    <>
+      <Card sx={{ maxWidth: 345 }}>
+        <CardHeader title="Maryam Mirzakhani" subheader="Matemática" />
+        <CardMedia
+          component="img"
+          height="194"
+          image="https://news.stanford.edu/wp-content/uploads/2017/07/Fields_Maryam_Mirzakhani.jpg"
+          alt="Maryam Mirzakhani"
+        />
         <CardContent>
-          <Typography paragraph style={{ fontWeight: "bold" }}>
-            História
-          </Typography>
-          <Typography paragraph>
-            Maryam Mirzakhani nasceu em Teerã, capital do Irã, onde também
-            cresceu. Apaixonada por leitura, concluiu o ensino fundamental
-            quando a guerra Irã-Iraque chegava ao fim e, em paralelo, as
-            oportunidades se abriam em seu país. Sendo assim, a aluna conquistou
-            uma vaga em uma prestigiada escola de ensino médio para meninas, que
-            desenvolvia “Talentos Excepcionais”.
-          </Typography>
-          <Typography paragraph>
-            No primeiro ano de estudos, curiosamente Mirzakhani foi mal na
-            matéria que mais tarde a consagraria. Foi no ano seguinte que as
-            coisas começaram a mudar. Seu desempenho melhorou muito e, sendo
-            assim, resolveu se inscrever na Olimpíada Internacional de
-            Matemática do Irã, que nunca havia tido uma garota entre os
-            competidores. Em 1994, aos 17 anos, sua pontuação na Olimpíada lhe
-            rendeu uma medalha de ouro. A partir de então, o amor pela matéria
-            só cresceu.
-          </Typography>
-          <Typography paragraph>
-            Se formou em matemática na Sharif University no Teerã em 1999 e fez
-            pós-graduação na universidade de Harvard. Foi lá que, durante um
-            seminário, ela se encantou com um assunto específico: a geometria
-            hiperbólica, que se baseia em superfícies em forma de "rosquinha"
-            com dois ou mais orifícios, que não seguem um padrão geométrico.
-          </Typography>
-          <Typography paragraph>
-            Mirzakhani foi professora de matemática na Universidade de Stanford
-            e muitos especialistas do ramo a consideram brilhante, pois suas
-            pesquisas se conectam a muitas áreas da matemática, incluindo
-            geometria diferencial, análise complexa e sistemas dinâmicos. Seu
-            trabalho resultou em diversos artigos publicados nas principais
-            revistas de matemática do mundo.
-          </Typography>
-          <Typography>
-            Em 2014, ela foi premiada com a Medalha Fields, por “suas
-            excepcionais contribuições à dinâmica e à geometria de superfícies
-            de Riemann e seus espaços de Moduli”. O prêmio é considerado como a
-            maior honraria que uma pessoa do ramo pode receber. Tanto é verdade,
-            que costuma ser chamada de “Prêmio Nobel da Matemática“.
+          <Typography variant="body2" color="text.secondary">
+            “Você tem que gastar um pouco de energia e esforço para ver a beleza
+            da matemática.”
           </Typography>
         </CardContent>
-      </Collapse>
-    </Card>
+        <CardActions disableSpacing>
+          <ExpandMore
+            expand={expanded}
+            onClick={handleExpandClick}
+            aria-expanded={expanded}
+            aria-label="show more"
+          >
+            <ExpandMoreIcon />
+          </ExpandMore>
+        </CardActions>
+        <Collapse in={expanded} timeout="auto" unmountOnExit>
+          <CardContent>
+            <Typography paragraph style={{ fontWeight: "bold" }}>
+              História
+            </Typography>
+            <Typography paragraph>
+              Maryam Mirzakhani nasceu em Teerã, capital do Irã, onde também
+              cresceu. Apaixonada por leitura, concluiu o ensino fundamental
+              quando a guerra Irã-Iraque chegava ao fim e, em paralelo, as
+              oportunidades se abriam em seu país. Sendo assim, a aluna
+              conquistou uma vaga em uma prestigiada escola de ensino médio para
+              meninas, que desenvolvia “Talentos Excepcionais”.
+            </Typography>
+            <Typography paragraph>
+              No primeiro ano de estudos, curiosamente Mirzakhani foi mal na
+              matéria que mais tarde a consagraria. Foi no ano seguinte que as
+              coisas começaram a mudar. Seu desempenho melhorou muito e, sendo
+              assim, resolveu se inscrever na Olimpíada Internacional de
+              Matemática do Irã, que nunca havia tido uma garota entre os
+              competidores. Em 1994, aos 17 anos, sua pontuação na Olimpíada lhe
+              rendeu uma medalha de ouro. A partir de então, o amor pela matéria
+              só cresceu.
+            </Typography>
+            <Typography paragraph>
+              Se formou em matemática na Sharif University no Teerã em 1999 e
+              fez pós-graduação na universidade de Harvard. Foi lá que, durante
+              um seminário, ela se encantou com um assunto específico: a
+              geometria hiperbólica, que se baseia em superfícies em forma de
+              "rosquinha" com dois ou mais orifícios, que não seguem um padrão
+              geométrico.
+            </Typography>
+            <Typography paragraph>
+              Mirzakhani foi professora de matemática na Universidade de
+              Stanford e muitos especialistas do ramo a consideram brilhante,
+              pois suas pesquisas se conectam a muitas áreas da matemática,
+              incluindo geometria diferencial, análise complexa e sistemas
+              dinâmicos. Seu trabalho resultou em diversos artigos publicados
+              nas principais revistas de matemática do mundo.
+            </Typography>
+            <Typography>
+              Em 2014, ela foi premiada com a Medalha Fields, por “suas
+              excepcionais contribuições à dinâmica e à geometria de superfícies
+              de Riemann e seus espaços de Moduli”. O prêmio é considerado como
+              a maior honraria que uma pessoa do ramo pode receber. Tanto é
+              verdade, que costuma ser chamada de “Prêmio Nobel da Matemática“.
+            </Typography>
+          </CardContent>
+        </Collapse>
+      </Card>
+
+      <Card sx={{ maxWidth: 345 }}>
+        <CardHeader title="Andrea Ghez" subheader="Astrofísica" />
+        <CardMedia
+          component="img"
+          height="194"
+          image="https://static.glamurama.uol.com.br/2020/10/site_andrea-ghez_elena-zhukova_ucla.jpg"
+          alt="Andrea Ghez"
+        />
+        <CardContent>
+          <Typography variant="body2" color="text.secondary">
+            “Se você tem paixão pela ciência, há muito para ser feito.”
+          </Typography>
+        </CardContent>
+        <CardActions disableSpacing>
+          <ExpandMore
+            expand={expanded}
+            onClick={handleExpandClick}
+            aria-expanded={expanded}
+            aria-label="show more"
+          >
+            <ExpandMoreIcon />
+          </ExpandMore>
+        </CardActions>
+        <Collapse in={expanded} timeout="auto" unmountOnExit>
+          <CardContent>
+            <Typography paragraph style={{ fontWeight: "bold" }}>
+              História
+            </Typography>
+            <Typography paragraph>
+              Andrea Ghez nasceu em Nova York, Estados Unidos, porém ainda
+              criança se mudou para Chicago onde frequentou a escola-laboratório
+              da Universidade de Chicago. Ghez queria ser inicialmente uma
+              bailarina, porém o pouso na lua a inspirou a querer tornar-se a
+              primeira mulher astronauta e sua mãe apoiou esta decisão, além de
+              sua professora ginasial de química. Começou a estudar matemática,
+              mas logo depois mudou seus interesses para física.
+            </Typography>
+            <Typography paragraph>
+              Obteve um bacharelado em física no Instituto de Tecnologia de
+              Massachusetts (MIT) em 1987 e obteve seu Ph.D. em 1992 no
+              Instituto de Tecnologia da Califórnia (Caltech). Suas pesquisas
+              atuais envolvem o uso de técnicas de imagem de alta resolução
+              espacial para estudar regiões de formação de estrelas e buracos
+              negros supermaciços. A maneira de "ver" um buraco negro, por
+              definição invisível, é observar os objetos que giram entorno e
+              revelam a presença do gigante.
+            </Typography>
+            <Typography paragraph>
+              Andrea ganhou o Prêmio Nobel de Física em 2020. O comitê da
+              premiação afirmou que os trabalhos dela, deram à ciência a
+              evidência mais convincente de um buraco negro supermassivo no
+              centro da Via Láctea.
+            </Typography>
+            <Typography paragraph>
+              Ela é a quarta mulher a ganhar um Nobel de Física desde que a
+              premiação começou, em 1901. A pesquisadora afirmou que pretende inspirar
+              outras mulheres a buscarem seus sonhos na profissão. "Há muito o
+              campo é dominado por homens, mas há cada vez mais mulheres
+              ingressando na disciplina. Estou muito feliz por poder ser um
+              modelo para as jovens que estão pensando em começar", conclui a
+              cientista.
+            </Typography>
+          </CardContent>
+        </Collapse>
+      </Card>
+    </>
   );
 }
